65045762e1b880952ae9b80f8d2fe18c8bdbc420
JacobDFrank <frankjacob42@gmail.com>
2018-12-27 22:31:00 -0500

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+const about = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>
+        This is about us text
+      </p>
+    </Layout>
+  );
+};
+
+export default about;
