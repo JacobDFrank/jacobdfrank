ed95e1044294f1affac96237d1ef1ced54f377f8
Jacob <jfrank@adobe.com>
2020-04-27 10:39:38 -0700

    Added footer new tab links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ class Footer extends React.Component {
       <footer>
         <div className="footer-container">
           <div className="courtesy">
-            <a href="mailto:frankjacob42@gmail.com?Subject=Hey%27s%20There" target="_top">Email Me</a>
-            <a href="https://www.jacobdfrank.com/media/Jacob_Frank_Resume.pdf">Resume</a>
-            <a href="https://github.com/JacobDFrank">Github</a>
-            <a href="https://www.linkedin.com/in/jacobdfrank">LinkedIn</a>
-            <a href="https://twitter.com/jacobdfrank">Twitter</a>
+            <a target="_blank" href="mailto:frankjacob42@gmail.com?Subject=Hey%27s%20There" target="_top">Email Me</a>
+            <a target="_blank" href="https://www.jacobdfrank.com/media/Jacob_Frank_Resume.pdf">Resume</a>
+            <a target="_blank" href="https://github.com/JacobDFrank">Github</a>
+            <a target="_blank" href="https://www.linkedin.com/in/jacobdfrank">LinkedIn</a>
+            <a target="_blank" href="https://twitter.com/jacobdfrank">Twitter</a>
           </div>
         </div>
       </footer>
