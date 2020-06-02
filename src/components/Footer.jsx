5f7a4cf6d8e7f3e828d36f33255c28a9da364c20
Jacob <jfrank@adobe.com>
2020-06-02 00:59:02 -0700

    designed better footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,17 @@ class Footer extends React.Component {
 
   render() {
     return (
-      <footer>
-        <div className="footer-container">
-          <div className="courtesy">
-            <a target="_blank" href="mailto:frankjacob42@gmail.com?Subject=Hey%27s%20There" target="_top">Email Me</a>
-            <a target="_blank" href="https://www.jacobdfrank.com/media/Jacob_Frank_Resume.pdf">Resume</a>
-            <a target="_blank" href="https://github.com/JacobDFrank">Github</a>
-            <a target="_blank" href="https://www.linkedin.com/in/jacobdfrank">LinkedIn</a>
-            <a target="_blank" href="https://twitter.com/jacobdfrank">Twitter</a>
+      <React.Fragment>
+        <footer className="footer--container">
+          <div className="footer--links">
+            <a className="footer--link" target="_blank" href="mailto:frankjacob42@gmail.com?Subject=Hey%27s%20There" className="footer--link" target="_top">Email</a>
+            <a className="footer--link" target="_blank" href="https://www.jacobdfrank.com/media/Jacob_Frank_Resume.pdf">Resume</a>
+            <a className="footer--link" target="_blank" href="https://github.com/JacobDFrank">Github</a>
+            <a className="footer--link" target="_blank" href="https://www.linkedin.com/in/jacobdfrank">LinkedIn</a>
+            <a className="footer--link" target="_blank" href="https://twitter.com/jacobdfrank">Twitter</a>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </React.Fragment>
     );
   }
 }
